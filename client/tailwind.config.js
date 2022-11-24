0eee00762aca00223175745a3f7e67b30749f183
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-25 02:03:06 +0300

    chore: extend tailwind with custom colors and font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["DM Sans", "sans-serif"],
+			},
+			colors: {
+				primary: "#8B80B6",
+				accent: "#F97474",
+				textDark: "#222",
+			},
+		},
 	},
 	plugins: [],
 };
