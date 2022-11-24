73f15f3587ae953f2a789d937f47bfabdc004ef0
Achufam24 <achuulimagbama@gmail.com>
2022-11-24 23:14:54 +0100

    project setup

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+const PORT = process.env.PORT || 8080
+
 const mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 require('dotenv').config();
@@ -12,14 +13,16 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
+//connect Database
 mongoose.connect(process.env.MONGO_URI, () => {
     console.log('Connected to MongoDb');
 });
 
+//Base route
 app.get('/', (req, res) => {
     res.send({mssg: "Welcome to Compartibility Checker"})
 });
 
 app.listen(3030, () => {
-    console.log(`server has started 'http://localhost:3030'`);
+    console.log(`server has started 'http://localhost:${PORT}'`);
 })
