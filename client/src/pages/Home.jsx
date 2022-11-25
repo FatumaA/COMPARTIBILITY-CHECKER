b464d415126e5756d9469371e14d45dd32d12dbc
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-25 18:07:27 +0300

    feat: add about section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,27 @@ const Home = (props) => {
 	return (
 		<div>
 			<Hero />
-			<div id="about">About</div>
+			<div
+				id="about"
+				className="flex flex-col container mx-auto text-center py-16 px-6"
+			>
+				<p className="pb-4 font-bold text-2xl">About</p>
+				<p>
+					We make the perfect couple with a few fun quiz questions to help you
+					see <br />
+					if your partner is a great match for you.
+				</p>
+				<p className="pt-8 max-w-3xl place-self-center">
+					From songs and poems to novels and movies, romantic love is one of the
+					most enduring subjects for artworks <br />
+					through the ages. But what about the science?
+					<br /> Historical, cultural and even evolutionary evidence suggests
+					love existed during ancient times and across many parts of the world.
+					Romantic love has been found to exist in 147 of 166 cultures looked at
+					in one study. The complexity of love has much to do with how people
+					experience it differently and how it can change over time.
+				</p>
+			</div>
 		</div>
 	);
 };
