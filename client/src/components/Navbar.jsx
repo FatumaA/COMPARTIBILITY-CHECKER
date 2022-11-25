7a285684183564fd07205075a09b4cbc9971daf5
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-11-25 07:48:11 +0300

    feat: set up basic routes for nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = (props) => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="flex flex-row justify-between mx-3 gap-4">
+			<Link to="/">Logo</Link>
+			<div className="flex flex-row justify-between gap-10">
+				<a href="/#about">About</a>
+				<Link to="login">Log In</Link>
+				<Link to="signup">Sign Up</Link>
+			</div>
+		</nav>
+	);
 };
 
 Navbar.propTypes = {};
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Home = (props) => {
+	return (
+		<div>
+			Home
+			<div id="about">About</div>
+		</div>
+	);
+};
+
+Home.propTypes = {};
+
+export default Home;
