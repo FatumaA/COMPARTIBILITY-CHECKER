5fddd614fafea4e51be2560d9f26f723cf4a68fe
Achufam24 <achuulimagbama@gmail.com>
2022-11-25 10:40:49 +0100

    feat: Oauth with google

---
+++ Server/models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type:String,
+    },
+    googleId:{
+        type:String,
+    },
+    thumbnail:{
+        type:String,
+    },
+    facebookId:{
+        type: String,
+    },
+    githubId:{
+        type:String
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
